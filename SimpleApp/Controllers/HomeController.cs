fcb8bf0286497e2b8c81f43521fc56dfbe34ecc6
sampanna pokhrel <sampanna99@gmail.com>
2016-10-10 17:09:01 -0500

    a simple app with webmail

--- SimpleApp/Controllers/HomeController.cs
+++ SimpleApp/Controllers/HomeController.cs
@@ -1,15 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using SimpleApp.Models;
+using System;
 using System.Web.Mvc;
 
 namespace SimpleApp.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ViewResult Index()
         {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
             return View();
         }
 
@@ -25,6 +25,26 @@ namespace SimpleApp.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Thanks", guestResponse);
+            }
+
+            else
+            {
+                return View();
+            }
         }
     }
 }
---
+++ SimpleApp/Models/GuestResponse.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SimpleApp.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "Please enter your name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter your email address")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Please enter a valid email address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please enter your Phone Number")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Please specify weather you going to attend")]
+        public bool? WillAttend { get; set; }
+    }
+}
